01e638e39b696f5be5e7cfa466531ed1eeac7139
klaglu <klaudia.gluszek@gmail.com>
2016-07-10 16:25:31 +0200

    Doanie elementu canvas o rozmiarze ekranu

---
+++ Retro-Balls/www/scripts/game.js
@@ -0,0 +1,10 @@
+
+function actualSize(){
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(95,50,40,0,2*Math.PI);
+  ctx.strokeStyle="#FFF";
+  ctx.stroke();
+
+};
