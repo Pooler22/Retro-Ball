5fb80ac3cfa45d8ede6dce85ff94230645e611fc
pooler22 <rudypawel12@gmail.com>
2016-07-10 16:39:19 +0200

    prepare convans

--- Retro-Balls/www/scripts/game.js
+++ Retro-Balls/www/scripts/game.js
@@ -1,10 +1,19 @@
+var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
 
-function actualSize(){
-  var canvas = document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
-  ctx.beginPath();
-  ctx.arc(95,50,40,0,2*Math.PI);
-  ctx.strokeStyle="#FFF";
-  ctx.stroke();
+    var imgData = ctx.createImageData(screen.width, screen.height);
+    var i;
+    for (i = 0; i < imgData.data.length; i += 4) {
+      imgData.data[i+0] = 102;
+      imgData.data[i+1] = 153;
+      imgData.data[i+2] = 255;
+      imgData.data[i+3] = 255;
+}
+    ctx.putImageData(imgData, 0, 0);
 
-};
+    ctx.strokeStyle="#FFF";
+    ctx.stroke();
+    alert("Width of imgData is: " + imgData.width);
