015340c209a230ec39f7efb8d041d09921c8bc02
pooler22 <rudypawel12@gmail.com>
2016-07-12 15:58:28 +0200

    demo technology

--- Retro-Balls/www/scripts/ball.js
+++ Retro-Balls/www/scripts/ball.js
@@ -18,8 +18,8 @@ class Ball {
     }
 
     addSpeed(x, y) {
-        this.speedX += 0.1* x;
-        this.speedY += 0.1* y;
+        this.speedX += 0.1 * x;
+        this.speedY += 0.1 * y;
         if (this.speedX > 10) {
             this.speedX = 10;
         } else if (this.speedX < -10) {
@@ -29,6 +29,26 @@ class Ball {
             this.speedY = -10;
         } else if (this.speedY < -10) {
             this.speedY = -10;
+        }
+    }
+    colisionInside(x, y, width, height) {
+        this.positionX -= this.speedX;
+        this.positionY -= this.speedY;
+        if (this.positionX - this.radius <= x) {
+            this.positionX = x + this.radius;
+            this.speedX -= 0.1;
+        }
+        if (this.positionX + this.radius >= width + x) {
+            this.positionX = width + x - this.radius;
+            this.speedX += 0.1;
+        }
+        if (this.positionY - this.radius <= y) {
+            this.positionY = y + this.radius;
+            this.speedY -= 0.1;
+        }
+        if (this.positionY + this.radius >= height + y) {
+            this.positionY = height + y - this.radius;
+            this.speedY += 0.1;
         }
     }
 }
--- Retro-Balls/www/scripts/game.js
+++ Retro-Balls/www/scripts/game.js
@@ -1,19 +1,110 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
+// jshint  esversion: 6
+var ctx, ball, hole;
 
-var imgData = ctx.createImageData(screen.width, screen.height);
-var i;
-for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i + 0] = 102;
-    imgData.data[i + 1] = 153;
-    imgData.data[i + 2] = 255;
-    imgData.data[i + 3] = 255;
+var canvas = document.getElementById('myCanvas');
+
+if (canvas.getContext) {
+
+    ctx = canvas.getContext('2d');
+    ctx.canvas.style.backgroundColor = "rgba(0,150,255,0.1)";
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    ball = new Ball(10, canvas.width / 2 , canvas.height / 2 - 5);
+    hole = new Hole(20, canvas.width / 2 + 10, canvas.height / 3 - 10);
+
+    var rightPressed = false;
+    var leftPressed = false;
+    var upPressed = false;
+    var downPressed = false;
+
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+    // document.addEventListener("mousemove", mouseMoveHandler, false)
+
+
+
+    render();
 }
-ctx.putImageData(imgData, 0, 0);
 
-ctx.strokeStyle = "#FFF";
-ctx.stroke();
-alert("Width of imgData is: " + imgData.width);
+function onSuccess(acceleration) {
+    ball.addSpeed(acceleration.x, -acceleration.y);
+    // console.log('Acceleration X: ' +  + '\n' +
+    //       'Acceleration Y: ' + acceleration.y + '\n');
+}
+
+function onError() {
+    console.log('onError!');
+}
+
+function keyDownHandler(e) {
+    switch (e.keyCode) {
+        case 37:
+            leftPressed = true;
+            ball.addSpeed(1, 0);
+            break;
+        case 38:
+            upPressed = true;
+            ball.addSpeed(0, 1);
+            break;
+        case 39:
+            rightPressed = true;
+            ball.addSpeed(-1, 0);
+            break;
+        case 40:
+            downPressed = true;
+            ball.addSpeed(0, -1);
+            break;
+    }
+}
+
+function keyUpHandler(e) {
+    switch (e.keyCode) {
+        case 37:
+            leftPressed = false;
+            ball.addSpeed(1, 0);
+            break;
+        case 38:
+            upPressed = false;
+            ball.addSpeed(0, 1);
+            break;
+        case 39:
+            rightPressed = false;
+            ball.addSpeed(-1, 0);
+            break;
+        case 40:
+            downPressed = false;
+            ball.addSpeed(0, -1);
+            break;
+    }
+}
+
+// function mouseMoveHandler(e) {
+//     var relativeX = e.clientX - canvas.offsetLeft;
+//     var relativeY = e.clientY - canvas.offsetTop;
+//     if (relativeX > 0 && relativeX < canvas.width && relativeY > 0 && relativeY < canvas.height) {
+//         ball.addSpeed((canvas.width / 2) - e.clientX, (canvas.height / 2) - e.clientY);
+//         // ball.positionX = relativeX - 100 / 2;
+//         // ball.positionY = relativeY - 100 / 2;
+//     }
+// }
+
+function render() {
+    update();
+    draw();
+    requestAnimationFrame(render);
+}
+
+
+function update() {
+    if (hole.colision(ball)) {
+        document.location.reload();
+    }
+    ball.colisionInside(0, 0, canvas.width, canvas.height);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball.draw();
+    hole.draw();
+}
